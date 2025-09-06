4a3edcba970c6ff862587b1605937b24c256aeb4
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-09-06 08:18:26 +0200

    add navigation structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
-  return <p> Test</p>;
+export default function Page() {
+  return (
+    <div className="m-5">
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+      <div className="mt-20 flex flex-col gap-2">
+        <Link href="/account">Account</Link>
+        <Link href="/cabins">Cabins</Link>
+        <Link href="/about">About</Link>
+      </div>
+    </div>
+  );
 }
