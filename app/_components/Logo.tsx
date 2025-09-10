47b0a8e0c8b4e482a7ce6c7ff85635424e7a89ff
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-09-10 14:30:10 +0200

    import logo img

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import LogoImg from "@/public/logo.png";
+
 function Logo() {
   return (
     <Link href="/" className="z-10 flex items-center gap-4">
-      <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      <Image src={LogoImg} alt="The Wild Oasis logo" width={60} />
       <span className="text-primary-100 text-xl font-semibold whitespace-nowrap">
         The Wild Oasis
       </span>
