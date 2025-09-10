c5fb560a8e9251da516e316ec6fa8fe2295a1142
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-09-10 12:25:23 +0200

    add custom google font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
-import type { Metadata } from "next";
 import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+import type { Metadata } from "next";
+
 import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
 
@@ -12,20 +15,24 @@ export const metadata: Metadata = {
     "Luxurious cabin hotel, located in the heart of the italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${josefinSans.className} antialiased`}>
       <body className="bg-primary-950 text-primary-100 flex min-h-screen flex-col">
         <header>
           <Logo />
         </header>
         <Navigation />
         <main className="flex-1">{children}</main>
-        <footer>Copyright by Daniel Muszkiet</footer>
+        <footer className="italic">Copyright by Daniel Muszkiet</footer>
       </body>
     </html>
   );
