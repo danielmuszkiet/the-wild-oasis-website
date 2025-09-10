caacc1812930bf43619cdd73470f1d7507819038
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-09-10 09:38:36 +0200

    ad date-fns package

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1 className="bg-primary-50">The Wild Oasis. Welcome to paradise.</h1>
+      <h1 className="bg-primary-500">The Wild Oasis. Welcome to paradise.</h1>
     </div>
   );
 }
