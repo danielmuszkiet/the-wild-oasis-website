bdadf7ebb29db04758c354e1ece89aac8a78f42d
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-09-14 16:51:27 +0200

    add cabins page

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,10 +1,32 @@
 import { Metadata } from "next";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Cabins",
 };
 
 export default function Page() {
-  return <Link href="/account">Account Link</Link>;
+  const cabins = [0, 1, 2];
+
+  return (
+    <div>
+      <h1 className="text-accent-400 mb-5 text-2xl font-medium md:text-4xl">
+        Our Luxury Cabins
+      </h1>
+      <p className="text-primary-200 mb-10 text-lg">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+      {cabins.length > 0 && (
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin) => {
+            return <span key={cabin}>Cabin Component</span>;
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
