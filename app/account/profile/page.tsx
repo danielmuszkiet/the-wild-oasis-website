b19e9b016204927e57349018639a32c133deb009
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-09-17 10:51:16 +0200

    temporary uncommenting for deployment

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -46,11 +46,11 @@ function Page() {
         <div className="space-y-1">
           <div className="flex justify-between">
             <label htmlFor="nationality">Where are you from?</label>
-            <img
+            {/* <img
               src={countryFlag}
               alt="Country flag"
               className="h-5 rounded-sm"
-            />
+            /> */}
           </div>
 
           <SelectCountry
