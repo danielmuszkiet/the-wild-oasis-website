c1223796f8cde21e63bb22213b37db07454e66c3
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-09-23 16:17:41 +0200

    fix date selection

--- app/_components/DateSelector.tsx
+++ app/_components/DateSelector.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { isWithinInterval } from "date-fns";
-import { DayPicker } from "react-day-picker";
+import { isWithinInterval, set } from "date-fns";
+import { DateRange, DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { Cabin, Settings } from "../_lib/definitions";
+import { useState } from "react";
 
 function isAlreadyBooked(range: { from: Date; to: Date }, datesArr: Date[]) {
   return (
@@ -24,17 +25,31 @@ function DateSelector({
   bookedDates: Date[];
   cabin: Cabin;
 }) {
+  const [range, setRange] = useState<DateRange>({
+    from: undefined,
+    to: undefined,
+  });
+
   const regularPrice = 23;
   const discount = 23;
   const numNights = 23;
   const cabinPrice = 23;
-  const range = { from: null, to: null };
 
   const { maxBookingLength, minBookingLength } = settings;
+
+  function handelDateRangeSelection(selectedRange: DateRange | undefined) {
+    if (!selectedRange) return;
+    setRange({ from: selectedRange.from, to: selectedRange.to });
+  }
+
+  function resetRange() {
+    setRange({ from: undefined, to: undefined });
+  }
 
   return (
     <div className="flex flex-col justify-between">
       <DayPicker
+        selected={range}
         className="m-auto flex w-full justify-center px-3 py-5 lg:py-0"
         mode="range"
         animate
@@ -48,6 +63,7 @@ function DateSelector({
         classNames={{
           months: "flex flex-col md:flex-row md:gap-5 md:w-full justify-center",
         }}
+        onSelect={(range) => handelDateRangeSelection(range)}
       />
 
       <div className="bg-accent-500 text-primary-800 md:gap:0 flex flex-col items-center justify-between gap-2 px-2 py-2 md:h-[72px] md:flex-row md:px-8 md:py-0">
@@ -83,8 +99,8 @@ function DateSelector({
         </div>
         {range.from || range.to ? (
           <button
-            className="border-primary-800 w-full border px-4 py-2 text-sm font-semibold md:w-auto"
-            // onClick={() => resetRange()}
+            className="border-primary-800 w-full cursor-pointer border px-4 py-2 text-sm font-semibold md:w-auto"
+            onClick={() => resetRange()}
           >
             Clear
           </button>
