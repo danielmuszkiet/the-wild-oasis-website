e002d2036ae4f3b86abdeb6102b4fe2f188c8abc
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-09-24 17:06:03 +0200

    add reminder

--- app/_components/ReservationReminder.tsx
+++ app/_components/ReservationReminder.tsx
@@ -5,18 +5,21 @@ import { format } from "date-fns";
 import { useReservation } from "@/app/_components/context/useReservation";
 
 function ReservationReminder() {
-  const { range } = useReservation();
+  const { range, resetRange } = useReservation();
 
   if (!range?.from || !range?.to) return null;
 
   return (
-    <div className="bg-accent-500 text-primary-800 text fixed bottom-6 left-1/2 flex -translate-x-1/2 items-center gap-8 rounded-full px-8 py-5 font-semibold shadow-xl shadow-slate-900">
-      <p>
-        <span>👋</span> Don&apos;t forget to reserve your dates <br /> from{" "}
+    <div className="bg-accent-500 text-primary-800 fixed bottom-5 left-1/2 flex w-[90%] -translate-x-1/2 items-center justify-center gap-5 rounded-3xl px-5 py-3 font-semibold shadow-xl shadow-slate-900 md:w-2xl md:rounded-full md:px-8 md:py-5">
+      <p className="text-center text-sm select-none md:text-base">
+        <span>👋</span> Don&apos;t forget to reserve your dates from{" "}
         {format(new Date(range.from), "MMM dd yyyy")} to{" "}
         {format(new Date(range.to), "MMM dd yyyy")}
       </p>
-      <button className="hover:bg-accent-600 cursor-pointer rounded-full p-1 transition-all">
+      <button
+        onClick={resetRange}
+        className="hover:bg-accent-600 cursor-pointer rounded-full p-1 transition-all"
+      >
         <XMarkIcon className="h-5 w-5" />
       </button>
     </div>
--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -4,6 +4,7 @@ import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
 import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
 // Not relevant because this page is dynamically rendered due to searchParams
 // export const revalidate = 10;
@@ -40,6 +41,7 @@ export default async function Page({
 
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
